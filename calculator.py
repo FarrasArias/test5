6b6ced5ac3099b3ffc5b4277d09204312ff082ce
FarrasArias <rafael.arias.glez@gmail.com>
2024-11-25 11:34:48 -0800

    Added Subtraction

--- calculator.py
+++ calculator.py
@@ -2,6 +2,11 @@ def add(x, y):
     """This function adds two numbers together."""
     return x + y
 
+def subtract(x, y):
+    """This function subtracts two numbers."""
+    return x * y
+
+
 if __name__ == "__main__":
     result = add(5, 3)
     print(f"The sum is: {result}")
